aa36ce1c0cf6b7afeafb14a02efa7196d0a78449
Kamil Kokot <kamil@kokot.me>
2017-02-14 23:37:36 +0100

    Include match ID in create match command

--- src/Match/Application/Command/CreateMatch.php
+++ src/Match/Application/Command/CreateMatch.php
@@ -4,25 +4,35 @@ declare(strict_types=1);
 
 namespace Pamil\Chess\Match\Application\Command;
 
+use Pamil\Chess\Match\Domain\Model\MatchId;
 use Pamil\Chess\Match\Domain\Model\PlayerId;
 
 final class CreateMatch
 {
+    /** @var MatchId */
+    private $matchId;
+
     /** @var PlayerId */
     private $whitePlayerId;
 
     /** @var PlayerId */
     private $blackPlayerId;
 
-    private function __construct(PlayerId $whitePlayerId, PlayerId $blackPlayerId)
+    private function __construct(MatchId $matchId, PlayerId $whitePlayerId, PlayerId $blackPlayerId)
     {
+        $this->matchId = $matchId;
         $this->whitePlayerId = $whitePlayerId;
         $this->blackPlayerId = $blackPlayerId;
     }
 
-    public static function betweenPlayers(PlayerId $whitePlayerId, PlayerId $blackPlayerId): self
+    public static function betweenPlayers(MatchId $matchId, PlayerId $whitePlayerId, PlayerId $blackPlayerId): self
     {
-        return new self($whitePlayerId, $blackPlayerId);
+        return new self($matchId, $whitePlayerId, $blackPlayerId);
+    }
+
+    public function matchId(): MatchId
+    {
+        return $this->matchId;
     }
 
     public function whitePlayerId(): PlayerId
--- src/Match/Application/CommandHandler/CreateMatchHandler.php
+++ src/Match/Application/CommandHandler/CreateMatchHandler.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Pamil\Chess\Match\Application\CommandHandler;
 
+use Broadway\CommandHandling\CommandHandlerInterface;
 use Pamil\Chess\Match\Application\Command\CreateMatch;
 use Pamil\Chess\Match\Domain\Model\Match;
 use Pamil\Chess\Match\Domain\Repository\MatchRepository;
 
-final class CreateMatchHandler
+final class CreateMatchHandler implements CommandHandlerInterface
 {
     /** @var MatchRepository */
     private $matchRepository;
@@ -20,8 +21,13 @@ final class CreateMatchHandler
 
     public function __invoke(CreateMatch $command): void
     {
-        $match = Match::create($command->whitePlayerId(), $command->blackPlayerId());
+        $match = Match::create($command->matchId(), $command->whitePlayerId(), $command->blackPlayerId());
 
         $this->matchRepository->add($match);
+    }
+
+    public function handle($command): void
+    {
+        $this($command);
     }
 }
