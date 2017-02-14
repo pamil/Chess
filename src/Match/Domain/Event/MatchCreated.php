1a590eaf901574ef3a7995d24dc0ca539d4bccb3
Kamil Kokot <kamil@kokot.me>
2017-02-14 22:16:58 +0100

    Add an event representing creating a match

---
+++ src/Match/Domain/Event/MatchCreated.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pamil\Chess\Match\Domain\Event;
+
+use Pamil\Chess\Match\Domain\Model\MatchId;
+use Pamil\Chess\Match\Domain\Model\PlayerId;
+
+final class MatchCreated
+{
+    /** @var MatchId */
+    private $matchId;
+
+    /** @var PlayerId */
+    private $whitePlayerId;
+
+    /** @var PlayerId */
+    private $blackPlayerId;
+
+    private function __construct(MatchId $matchId, PlayerId $whitePlayerId, PlayerId $blackPlayerId)
+    {
+        $this->matchId = $matchId;
+        $this->whitePlayerId = $whitePlayerId;
+        $this->blackPlayerId = $blackPlayerId;
+    }
+
+    public static function betweenPlayers(MatchId $matchId, PlayerId $whitePlayerId, PlayerId $blackPlayerId): self
+    {
+        return new self($matchId, $whitePlayerId, $blackPlayerId);
+    }
+
+    public function matchId(): MatchId
+    {
+        return $this->matchId;
+    }
+
+    public function whitePlayerId(): PlayerId
+    {
+        return $this->whitePlayerId;
+    }
+
+    public function blackPlayerId(): PlayerId
+    {
+        return $this->blackPlayerId;
+    }
+}
