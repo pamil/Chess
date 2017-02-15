4d0de553b3b8d02e1b90549fab0736f8161ae215
Kamil Kokot <kamil@kokot.me>
2017-02-16 00:03:19 +0100

    Add basic operations on Elo score

--- src/Player/Domain/Model/Elo.php
+++ src/Player/Domain/Model/Elo.php
@@ -6,6 +6,8 @@ namespace Pamil\Chess\Player\Domain\Model;
 
 final class Elo
 {
+    private const K = 32;
+
     /** @var int */
     private $score;
 
@@ -18,8 +20,36 @@ final class Elo
         $this->score = $score;
     }
 
+    public static function getProbabilityOfWinning(self $subject, self $opponent): float
+    {
+        $transformedOfScore = 10 ** ($subject->score / 400);
+        $transformedVersusScore = 10 ** ($opponent->score / 400);
+
+        return $transformedOfScore / ($transformedOfScore + $transformedVersusScore);
+    }
+
+    public function afterWinningAgainst(self $opponent): self
+    {
+        return new self($this->score + $this->getDelta(1, self::getProbabilityOfWinning($this, $opponent)));
+    }
+
+    public function afterDrawingAgainst(self $opponent): self
+    {
+        return new self($this->score + $this->getDelta(0.5, self::getProbabilityOfWinning($this, $opponent)));
+    }
+
+    public function afterLosingAgainst(self $opponent): self
+    {
+        return new self($this->score + $this->getDelta(0, self::getProbabilityOfWinning($this, $opponent)));
+    }
+
     public function toInt(): int
     {
         return $this->score;
+    }
+
+    private function getDelta(float $result, float $probability): int
+    {
+        return (int) floor(self::K * ($result - $probability));
     }
 }
--- tests/Player/Domain/Model/EloTest.php
+++ tests/Player/Domain/Model/EloTest.php
@@ -17,6 +17,40 @@ final class EloTest extends TestCase
         Assert::assertSame(0, $elo->toInt());
     }
 
+    /** @test */
+    public function it_computes_score_after_winning()
+    {
+        Assert::assertSame(1520, (new Elo(1500))->afterWinningAgainst(new Elo(1600))->toInt());
+        Assert::assertSame(1516, (new Elo(1500))->afterWinningAgainst(new Elo(1500))->toInt());
+        Assert::assertSame(1511, (new Elo(1500))->afterWinningAgainst(new Elo(1400))->toInt());
+    }
+
+    /** @test */
+    public function it_computes_score_after_drawing()
+    {
+        Assert::assertSame(1504, (new Elo(1500))->afterDrawingAgainst(new Elo(1600))->toInt());
+        Assert::assertSame(1500, (new Elo(1500))->afterDrawingAgainst(new Elo(1500))->toInt());
+        Assert::assertSame(1495, (new Elo(1500))->afterDrawingAgainst(new Elo(1400))->toInt());
+    }
+
+    /** @test */
+    public function it_computes_score_after_losing()
+    {
+        Assert::assertSame(1488, (new Elo(1500))->afterLosingAgainst(new Elo(1600))->toInt());
+        Assert::assertSame(1484, (new Elo(1500))->afterLosingAgainst(new Elo(1500))->toInt());
+        Assert::assertSame(1479, (new Elo(1500))->afterLosingAgainst(new Elo(1400))->toInt());
+    }
+
+    /** @test */
+    public function it_is_immutable()
+    {
+        $elo = new Elo(1000);
+
+        Assert::assertNotSame($elo, $elo->afterWinningAgainst(new Elo(1000)));
+        Assert::assertNotSame($elo, $elo->afterDrawingAgainst(new Elo(1000)));
+        Assert::assertNotSame($elo, $elo->afterLosingAgainst(new Elo(1000)));
+    }
+
     /**
      * @test
      *
